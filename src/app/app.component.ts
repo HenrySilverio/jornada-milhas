cbed6c6521543312043389fad745ab14a09d778e
HenrySilverio <silveriohenriqueb@gmail.com>
2025-07-24 08:03:21 -0300

    Refactors layout and integrates header and banner components
    Replaces placeholder content with reusable header and banner components.
    Improves styling consistency and adds input properties for flexibility.
    Updates application bootstrap configuration to include routing, animations, and hydration support.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule }    from '@angular/common';
+import { RouterOutlet }    from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { BannerComponent } from "./shared/banner/banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    BannerComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'jornada-milhas';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter }        from '@angular/router';
+import { provideAnimations }    from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { routes }       from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideClientHydration(),
+    provideRouter(routes),
+    provideAnimations()
+  ]
+})
+.catch(err => console.error(err));
